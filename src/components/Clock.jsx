52aa12361143f7a42f383b7673ff9712d72eb9b7
Pugga <aezakmi2508@gmail.com>
2022-07-15 15:59:53 +0300

    First Version

---
+++ src/components/Clock.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Clock () {
+
+    let data = new Date();
+    let hours = data.getHours();
+    let min = data.getMinutes();
+    let sec = data.getSeconds();
+    let day = data.getDay();
+    let mon = data.getMonth();
+    let year = data.getFullYear();
+    
+    return (
+        
+         <div>
+            <h1>{hours}:{min}:{sec}</h1>
+            <h2>{day}:{mon}:{year}</h2>
+        </div>  
+    )}
+    
+setInterval(Clock, 1000);
+export default Clock;
