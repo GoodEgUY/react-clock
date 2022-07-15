f1b0f84c7a7d52032da809d26b9bbff2ccac2fc8
Pugga <aezakmi2508@gmail.com>
2022-07-15 17:10:23 +0300

    Added clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import Clock from './components/Clock';
-
+import React from "react";
+import "./App.css";
+import Clock from "./components/Clock";
 
 function App() {
   return (
     <div className="App">
-      <Clock />
+      <div className="wrapper">
+        <div className="content">
+          <Clock />
+          
+        </div>
+      </div>
     </div>
   );
 }
