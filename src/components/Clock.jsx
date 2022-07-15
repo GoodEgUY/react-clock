9923aee0d983fe8a2f866e9216011105e4f5756f
Pugga <aezakmi2508@gmail.com>
2022-07-15 17:10:39 +0300

    Upgrade component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,20 +1,24 @@
+function Clock() {
+  let data = new Date();
+  let hours = data.getHours();
+  let min = data.getMinutes();
+  let sec = data.getSeconds();
+  let day = data.getDay();
+  let mon = data.getMonth();
+  let year = data.getFullYear();
 
-function Clock () {
+  return (
+    <div className="clockScreen">
+      <div className="clock">
+        <h1>
+          {hours}:{min}:{sec}
+        </h1>
+        <h2>
+          {day}:{mon}:{year}
+        </h2>
+      </div>
+    </div>
+  );
+}
 
-    let data = new Date();
-    let hours = data.getHours();
-    let min = data.getMinutes();
-    let sec = data.getSeconds();
-    let day = data.getDay();
-    let mon = data.getMonth();
-    let year = data.getFullYear();
-    
-    return (
-        
-         <div>
-            <h1>{hours}:{min}:{sec}</h1>
-            <h2>{day}:{mon}:{year}</h2>
-        </div>  
-    )}
-    
 export default Clock;
